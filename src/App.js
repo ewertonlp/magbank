927815e53859d9cca525f0d50fee6369bd9894c2
Ewerton Lopes Pereira <ewertonlopespereira@gmail.com>
2021-04-15 17:28:06 -0300

    feat(react): Navbar and Hero.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Main from "./components/Main";
 import "./App.scss";
+
 
 const App = () => (
     <div className="App">
       <Navbar />
+      <Hero />
+      <Main />
     </div>
   );
 
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import "./Main.scss";
+
+const Main = () => (
+  <Container>
+    <Row>
+      <Col>
+        <h2>Cartão de Crédito</h2>
+        
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Main;
